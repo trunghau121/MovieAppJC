de1ee5ef5df6664be85f8255daccb59a7f385b6b
trunghau121 <trunghau121@gmail.com>
2024-01-11 13:34:09 +0700

    -Update version lib

--- app/src/main/java/com/movieappjc/presentation/components/CustomTabBar.kt
+++ app/src/main/java/com/movieappjc/presentation/components/CustomTabBar.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -75,10 +76,11 @@ private fun MyTabItem(
     onClick: () -> Unit,
     title: String
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
     Box(
         modifier = modifier
             .clickable(
-                interactionSource = MutableInteractionSource(),
+                interactionSource = interactionSource,
                 indication = null
             ) { onClick() },
         contentAlignment = Alignment.Center
